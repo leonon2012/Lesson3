f47b0d43269ada8f5d832bdcc33156d52c834898
leonon2012 <leonon2012@gmail.com>
2019-05-23 15:45:52 +0300

    v2

--- Program.cs
+++ Program.cs
@@ -6,9 +6,19 @@ namespace Lesson3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Task57(3.3, 9.9));
+            // Test 57
+            Console.WriteLine(Task57(0.000541, 9.9));
+
+            // Test 164
+            Console.WriteLine(Task164(1, 2, 3));
+            Console.WriteLine(Task164(1, 3, 2));
+            Console.WriteLine(Task164(2, 1, 3));
+            Console.WriteLine(Task164(2, 3, 1));
+            Console.WriteLine(Task164(3, 1, 2));
+            Console.WriteLine(Task164(3, 2, 1));
         }
 
+        // Task 57
         static string Task57(double a, double b)
         {
             if (a == 0)
@@ -16,6 +26,24 @@ namespace Lesson3
             else
                 return $"Число {a}" + (b % a <= 0.0001 ? " " : " не ") + $"является делителем числа {b}";
         }
+
+        // Task 164
+        static string Task164(double a, double b, double c)
+        {
+            double minV, avgV, maxV;
+
+            minV = Math.Min(Math.Min(a, b), c);
+            maxV = Math.Max(Math.Max(a, b), c);
+
+            if (maxV == a)
+                avgV = Math.Max(b, c);
+            else
+                avgV = Math.Max(Math.Min(b, c), a);
+
+            return $"Min={minV}, Avg={avgV}, Max={maxV}";
+        }
+
+
     }
 }
 
