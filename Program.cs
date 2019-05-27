4e3834cdd15ce8780c475991256834c6240dc8a4
leonon2012 <leonon2012@gmail.com>
2019-05-27 09:17:49 +0300

    v3

--- Program.cs
+++ Program.cs
@@ -16,6 +16,9 @@ namespace Lesson3
             Console.WriteLine(Task164(2, 3, 1));
             Console.WriteLine(Task164(3, 1, 2));
             Console.WriteLine(Task164(3, 2, 1));
+
+            // Task33
+            Task33();
         }
 
         // Task 57
@@ -43,7 +46,37 @@ namespace Lesson3
             return $"Min={minV}, Avg={avgV}, Max={maxV}";
         }
 
+        static void Task33()
+        {
+            double r1 = 0, r2 = 0;
 
+            ReadParams(ref r1, "R1");
+            ReadParams(ref r2, "R2");
+
+            Console.WriteLine("1/R1 + 1/R2 = " + (1/r1 + 1/r2));
+
+            void ReadParams(ref double param, string paramName)
+            {
+                // Запрашиваем число
+                Console.Write("Укажите число "+ paramName + ": ");
+                string line = Console.ReadLine();
+
+                try
+                {
+                    param = Convert.ToDouble(line);
+                    if (param == 0)
+                    {
+                        Console.WriteLine("Значение не может быть равным 0, попробуйте еще раз!");
+                        ReadParams(ref param, paramName);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine(line + " не является числом, попробуйте еще раз!");
+                    ReadParams(ref param, paramName);
+                }
+            }            
+        }
     }
 }
 
